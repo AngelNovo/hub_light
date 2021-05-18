a8c6af9dcef80c68c7117216c84f2a4376b478e3
Joan <joanllabres@paucasesnovescifp.cat>
2021-05-18 20:51:43 +0200

    contingut

--- resources/views/front/contenido.blade.php
+++ resources/views/front/contenido.blade.php
@@ -36,8 +36,24 @@
     @endif
     <div class="footer-contingut">
         <div class="header-footer-contingut">
-            <i class="fa pe-7s-like" data-toggle="Me gusta"> </i>
-            <i class="fa pe-7s-paper-plane" title="Enviar"> </i>
+            <div>
+                <i class="fa pe-7s-like" data-toggle="Me gusta"> </i>
+                <i class="fa pe-7s-paper-plane" title="Enviar"> </i>
+            </div>            
+        </div>
+        <div class="border-bot">
+        </div>
+        <div >
+            @if (isset(Auth::user()->id)&&Auth::user()->id!=$results->id_user)
+                <form id="comentari">
+                    <div class="form-group">
+                        <img src={{asset("images/perfil/usuarios/".Auth::user()->foto)}} class="foto-coment">
+                        <input type="checkbox" hidden name="megusta" id="megusta">
+                        <textarea class="form-control" rows="4" name="mensaje" id="mensaje"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary" id="submit-comment">Envia</button>
+                </form>
+            @endif 
         </div>
     </div>        
 
@@ -48,6 +64,51 @@
     $(document).ready(function(){
         // Marcar Navbar
         $(".isSelected").removeClass("isSelected");
+        var megusta=$("#megusta").val();
+        $(document).on("submit",function(e){
+            e.preventDefault();
+            enviaComent();
+        });
     });
+
+    function enviaComent(){
+        var comentario=$("#mensaje").val();
+        $.ajax({
+        url: "/comment",
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        type: "POST",
+        dataType: 'json',
+        data: {
+            "id_contingut":{{$results->id}},
+            "comentario":comentario
+        },
+        success: function(data){                   
+          $.each(data[0], function(index,element){
+            var option=$("<option>");
+            option.val("@"+element.name);
+            option.attr("data-id",element.id);
+            $("#cercador").append(option);
+          });   
+          $.each(data[1], function(index,element){
+            var option=$("<option>");
+            option.val("-"+element.titulo);
+            option.attr("data-id",element.id);
+            $("#cercador").append(option);
+          });    
+          $.each(data[2], function(index,element){
+            var option=$("<option>");
+            option.val("#"+element.nombre);
+            option.attr("data-id",element.id);
+            $("#cercador").append(option);
+          });  
+        }
+        });
+    }
+    
+    //id_contenido
+    //megusta
+    //comentario
 </script>
 @endsection

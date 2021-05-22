a6dc6a0b3b40d0af2516444cd10119ad7ef6b080
AngelNovo <angelnovo15@gmail.com>
2021-05-22 17:21:29 +0200

    s

--- app/Http/Controllers/InteraccioController.php
+++ app/Http/Controllers/InteraccioController.php
@@ -59,7 +59,7 @@ class InteraccioController extends Controller
         //4: devolvemos una respuesta response() con el valor que queramos
     }
     public function getComments($id) {
-        $comments = InteraccioModel::where('id_contingut',$id);
+        $comments = InteraccioModel::where('id_contingut',$id)->get();
         return $comments;
     }
 }

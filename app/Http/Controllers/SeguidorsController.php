fa6bf5e61b2fdcc1373b281eaa3d7b6b1bee56e3
AngelNovo <angelnovo15@gmail.com>
2021-05-25 23:05:20 +0200

    s

--- app/Http/Controllers/SeguidorsController.php
+++ app/Http/Controllers/SeguidorsController.php
@@ -43,7 +43,7 @@ class SeguidorsController extends Controller
         foreach($interaccions as $i) {
             $i->tipo="interaccion";
         }
-        $avis_usuari=AvisUsuariModel::where(['id_usuari'=>Auth::user()->id,"acceptat"=>1])
+        $avis_usuari=AvisUsuariModel::where(['id_usuari'=>Auth::user()->id,"acceptat"=>1,"removed"=>0])
         ->join("avis","avis.id","=","id_avis")
         ->get();
         foreach($avis_usuari as $a) {

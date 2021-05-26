f645f33b82e5d7f7e4fde59916f7f58ac6261e70
Joan <joanllabres@paucasesnovescifp.cat>
2021-05-26 20:58:49 +0200

    gfhfg

--- resources/views/auth/canviarPassword.blade.php
+++ resources/views/auth/canviarPassword.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Restablecer Contraseña') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -15,13 +15,8 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo Electronico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control" name="email" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
@@ -29,17 +24,11 @@
                             <label for="user" class="col-md-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
 
                             <div class="col-md-6">
-                                <input id="user" type="text" class="form-control @error('user') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('user')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="user" type="text" class="form-control " name="password" required autocomplete="current-password">
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <button type="submit" class="btn btn-primary">
+                        <div class="form-group row justify-content-center mt-3">
+                            <button type="button" class="btn btn-primary resetPassword">
                                 {{ __('Recordar mi contraseña') }}
                             </button>
                         </div>
@@ -50,4 +39,36 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function(){
+        $(".resetPassword").on("click",function(){
+            verifica($("#email").val(),$("#user").val());
+        });
+    });
+
+    function verifica(email,name){
+    
+    $.ajax({
+        url: "/generapassword/vista",
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        type: "POST",
+        data: {
+            "email":email,
+            "name":name
+        },
+        success: function(data){
+           if(data){
+               alert("La contraseña se ha canviado correctamente, revisa el correo electronico");
+               window.location="/login";
+           }else{
+                alert("El email o el usuario no coinciden");
+           }
+        },error: function(data){
+           console.log(data);
+        }
+      });
+  }
+</script>
 @endsection
